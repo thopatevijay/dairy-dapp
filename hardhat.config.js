9470e06b8ee63a3d09bb4550f1a96a9a30cc8646
thopatevijay <thopatevijay@gmail.com>
2023-01-15 14:43:07 +0530

    connecting to goril

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
-  defaultNetwork: 'ganache',
+  defaultNetwork: 'goril',
   networks: {
     ganache: {
       url: 'http://localhost:8545'
+    },
+    goril: {
+      url: `https://goerli.infura.io/v3/ee57b3bb82a14b27b3951b499c708b5c`,
+      accounts: [`0x` + process.env.PRIVATE_KEY],
     }
   },
   solidity: "0.8.17",
