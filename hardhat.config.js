5bfabf81fa0e3fe2035c9a43d7e52b6df665c1ec
thopatevijay <thopatevijay@gmail.com>
2023-01-15 14:44:21 +0530

    connecting to goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,12 +4,12 @@ require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 
 module.exports = {
-  defaultNetwork: 'goril',
+  defaultNetwork: 'goerli',
   networks: {
     ganache: {
       url: 'http://localhost:8545'
     },
-    goril: {
+    goerli: {
       url: `https://goerli.infura.io/v3/ee57b3bb82a14b27b3951b499c708b5c`,
       accounts: [`0x` + process.env.PRIVATE_KEY],
     }
