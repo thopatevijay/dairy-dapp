fe4dcd87077c9e4188e6a4d8fd8f409ee9387bc9
thopatevijay <thopatevijay@gmail.com>
2023-01-18 13:08:31 +0530

    form toggle

--- pages/MilkCollector.jsx
+++ pages/MilkCollector.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { useUserContext } from '../common/Provider';
 import AccessDenied from "./components/AccessDenied";
@@ -6,6 +7,7 @@ import MilkCollectTable from './components/MilkCollectTable';
 import MilkCollectForm from './components/MilkCollectForm';
 
 const MilkCollector = () => {
+    const [formVisible, setFormVisible] = useState(false)
     const { user } = useUserContext();
 
     const router = useRouter();
@@ -17,19 +19,30 @@ const MilkCollector = () => {
         return <AccessDenied requiredRole="Milk Collector" />
     }
 
+    const handleToggleForm = () => {
+        setFormVisible(!formVisible);
+    }
+
     return (
         <main className='py-5'>
             <h1 className='text-xl md:text-2xl text-center font-bold'>Milk Collection</h1>
             <div className="container mx-auto flex justify-between py-5 border-b">
-                <div className="left flex gap-3">
+                <div className="left flex gap-3" onClick={handleToggleForm}>
                     <button className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800">
                         Collect Milk <span className='px-1'><BiUserPlus size={23}></BiUserPlus></span>
                     </button>
                 </div>
             </div>
-            <div className="container mx-auto py-5">
-                <MilkCollectForm />
-            </div>
+            {
+                formVisible
+                    ?
+                    (
+                        <div className="container mx-auto py-5">
+                            <MilkCollectForm />
+                        </div>
+                    )
+                    : null
+            }
             <div className="container mx-auto">
                 <MilkCollectTable />
             </div>
