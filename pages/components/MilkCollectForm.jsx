fa15698b0c2f81f19a2e454d9065dcbdf3d6043a
thopatevijay <thopatevijay@gmail.com>
2023-01-21 11:49:35 +0530

    collect milk form functionality done

--- pages/components/MilkCollectForm.jsx
+++ pages/components/MilkCollectForm.jsx
@@ -1,41 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const MilkCollectForm = () => {
+    const [id, setId] = useState('');
+    const [milkAmount, setMilkAmount] = useState('');
+    const [milkQuality, setMilkQuality] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:3000/api/milk-collector', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ id, milkAmount, milkQuality }),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+            } else {
+                const data = await response.json();
+                setError(data.error);
+            }
+        } catch (err) {
+            console.error(err);
+            setError('An error occurred. Please try again later.');
+        }
+    };
+
     return (
-        <form className="grid lg:grid-cols-2 w-4/6 gap-4">
-            <div className="input-type">
-                <input type="text" name="firstname" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="FirstName" />
-            </div>
-            <div className="input-type">
-                <input type="text" name="firstname" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="LastName" />
-            </div>
-            <div className="input-type">
-                <input type="text" name="email" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Email" />
-            </div>
-            <div className="input-type">
-                <input type="text" name="salary" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Salary" />
-            </div>
-            <div className="input-type">
-                <input type="date" name="date" className="border px-5 py-3 focus:outline-none rounded-md" placeholder="Salary" />
-            </div>
+        <form className="grid lg:grid-cols-2 w-4/6 gap-4" onSubmit={handleSubmit}>
+            {error && <p className="text-red-500">{error}</p>}
 
-
-            <div className="flex gap-10 items-center">
-                <div className="form-check">
-                    <input type="radio" value="Active" id="radioDefault1" name="status" className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300  bg-white checked:bg-green-500 checked:border-green-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" />
-                    <label htmlFor="radioDefault1" className="inline-block tet-gray-800">
-                        Active
-                    </label>
-                </div>
-                <div className="form-check">
-                    <input type="radio" value="Inactive" id="radioDefault2" name="status" className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300  bg-white checked:bg-green-500 checked:border-green-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" />
-                    <label htmlFor="radioDefault2" className="inline-block tet-gray-800">
-                        Inactive
-                    </label>
-                </div>
+            <div className="input-type">
+                <input type="text" value={id} onChange={(e) => setId(e.target.value)} name="farmerID" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Farmer ID" />
             </div>
-
-            <button className="flex justify-center text-md w-2/6 bg-green-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-green-500 hover:text-green-500">Add</button>
+            <div className="input-type">
+                <input type="text" value={milkAmount} onChange={(e) => setMilkAmount(e.target.value)} name="milkAmount" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Milk Amount" />
+            </div>
+            <div className="input-type">
+                <input type="text" value={milkQuality} onChange={(e) => setMilkQuality(e.target.value)} name="milkQuality" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Milk Quality" />
+            </div>
+            <button className="flex justify-center text-md w-2/6 bg-green-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-green-500 hover:text-green-500">Collect Milk</button>
 
         </form>
     )
