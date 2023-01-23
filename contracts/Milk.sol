c227442a52b55b8c81f6d740c7f04c2d37f45765
thopatevijay <thopatevijay@gmail.com>
2023-01-23 14:08:41 +0530

    created batch adding functionality in contract

--- contracts/Milk.sol
+++ contracts/Milk.sol
@@ -21,16 +21,35 @@ contract Milk {
         uint256 timestamp;
     }
 
+    // Struct to represent a Batch by milk collector
+    struct BatchByCollector {
+        uint256 batchId;
+        uint256 collectorId;
+        uint256 batchCreatedTime;
+        uint256[] collectionIds;
+        uint256 quantity;
+        uint256 quality;
+        bool accepted;
+        uint256 statusUpdateTime;
+    }
+
     // Mapping to store the farmers
     mapping(uint256 => Farmer) public farmers;
+
     // Mapping to store the milk collections
     mapping(uint256 => MilkCollection) public milkCollections;
+
+    // Mapping to store the batches
+    mapping(uint256 => BatchByCollector) public collectorBatches;
 
     // Counter for assigning unique IDs to farmers
     uint256 public farmerIdCounter = 1;
 
     // Counter for assigning unique IDs to milk collections
     uint256 public milkCollectionCounter = 1;
+
+    // Counter for assigning unique IDs to collectorBatches
+    uint256 public collectorBatchIdCounter = 1;
 
     // Function to add a farmer to the contract
     function addFarmer(uint256 milkCollectorId, string memory name) public {
@@ -60,5 +79,45 @@ contract Milk {
             block.timestamp
         );
         milkCollectionCounter++;
+    }
+
+    // Function to create a batch by milk collectors
+    function createMilkCollectorBatch(
+        uint256 collectorId,
+        uint256[] memory collectionIds,
+        uint256 quantity,
+        uint256 quality
+    ) public {
+        // Create a batch struct and add it to the batches mapping
+        uint256 batchId = collectorBatchIdCounter;
+        collectorBatchIdCounter++;
+        collectorBatches[batchId] = BatchByCollector(
+            batchId,
+            collectorId,
+            block.timestamp,
+            collectionIds,
+            quantity,
+            quality,
+            false,
+            block.timestamp
+        );
+    }
+
+    // Function to get collections ID's from batches
+    function getBatchCollectionIds(uint256 batchId)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        return collectorBatches[batchId].collectionIds;
+    }
+
+    // Function to update the accepted status of milk collector batch
+    function updateMilkCollectorBatchStatus(uint256 batchId, bool newStatus)
+        public
+    {
+        BatchByCollector storage batch = collectorBatches[batchId];
+        batch.accepted = newStatus; //update the accepted status
+        batch.statusUpdateTime = block.timestamp; // update the timestamp
     }
 }
