24d8360c3f17f37df9c6bde48212d232766f15c5
thopatevijay <thopatevijay@gmail.com>
2023-01-29 23:01:13 +0530

    #17 rendering all component in dashboard page

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,8 +1,14 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { useUserContext } from '../common/Provider';
+import MilkCollector from './modules/MilkCollector';
+import MilkProcessor from './modules/MilkProcessor';
+import Distributor from './modules/Distributor';
+import Retailer from './modules/Retailer';
 
 const Dashboard = () => {
     const router = useRouter();
+    const { user } = useUserContext();
 
     useEffect(() => {
         const userData = JSON.parse(localStorage.getItem('dairy-login'));
@@ -12,7 +18,16 @@ const Dashboard = () => {
     }, [router]);
 
     return (
-        <div className="bg-gray-200 min-h-screen flex flex-col justify-center items-center">
+        <div>
+            {user?.role === "milkcollector"
+                ? <MilkCollector />
+                : user?.role === 'milkprocessor'
+                    ? <MilkProcessor />
+                    : user?.role === 'distributor'
+                        ? <Distributor />
+                        : <Retailer />
+            }
+
         </div>
     );
 };
